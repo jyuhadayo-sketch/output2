aa6e31d2dff40eea372a6d49eb2440952c24a462
jyuhadayo-sketch <jyuhadayo@gmail.com>
2025-12-06 15:56:30 +0800

    add pin

--- skin.js
+++ skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 7.1.11/21010
 // Filename: mapskin.ggsk
-// Generated 2025-12-06T15:53:22
+// Generated 2025-12-06T15:55:56
 
 function pano2vrSkin(player,base) {
 	var me=this;
